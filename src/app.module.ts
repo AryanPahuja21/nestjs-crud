7db1f8605d0efa125ce255d9404a74bfbb9cf13d
AryanPahuja21 <aryanpahuja1421@gmail.com>
2025-10-08 14:41:31 +0530

    chore: complete initial project setup with base dependencies and folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
-import { ProductModule } from './product/product.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    // MySQL connection
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'nestuser',
-      password: 'nestpass',
-      database: 'saas',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    // MongoDB connection
-    MongooseModule.forRoot('mongodb://localhost:27017/saas'),
-    UserModule,
-    ProductModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    // TypeOrm & Mongoose modules will be added in DB section
   ],
 })
 export class AppModule {}
